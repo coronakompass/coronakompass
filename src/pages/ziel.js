f954eeeed2f9d64c1d75f940ed6b45c9705bbe42
Christoph Tavan <dev@tavan.de>
2020-03-22 21:59:31 +0100

    Map places types

--- src/pages/ziel.js
+++ src/pages/ziel.js
@@ -3,20 +3,25 @@ import Box from '@material-ui/core/Box';
 import Button from '@material-ui/core/Button';
 import Container from '@material-ui/core/Container';
 import CheckIcon from '@material-ui/icons/Check';
-import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
+import CallMadeIcon from '@material-ui/icons/CallMade';
 import ArrowBackIosIcon from '@material-ui/icons/ArrowBackIos';
 import Typography from '@material-ui/core/Typography';
 import Grid from '@material-ui/core/Grid';
 import LinearProgress from '@material-ui/core/LinearProgress';
 import { makeStyles } from '@material-ui/core/styles';
 import { useRouter } from 'next/router';
+import clsx from 'clsx';
 import Footer from '../components/Footer';
 import Link from '../components/Link';
+import placeTypes from '../placeTypes';
 
 const useStyles = makeStyles((theme) => ({
   grid: {
     marginTop: theme.spacing(2),
     marginBottom: theme.spacing(2),
+  },
+  buttonContainer: {
+    marginTop: theme.spacing(4),
   },
   icon: {
     color: theme.palette.text.primary,
@@ -26,13 +31,18 @@ const useStyles = makeStyles((theme) => ({
     marginLeft: theme.spacing(1),
     color: theme.palette.success.dark,
   },
-  yay: {
+  status: {
     marginTop: theme.spacing(4),
     marginBottom: theme.spacing(4),
-    backgroundColor: theme.palette.success.light,
     padding: theme.spacing(2),
     width: `calc(100% + ${theme.spacing(4)}px)`,
     marginLeft: -1 * theme.spacing(2),
+  },
+  yay: {
+    backgroundColor: theme.palette.success.light,
+  },
+  nay: {
+    backgroundColor: theme.palette.error.light,
   },
 }));
 
@@ -62,6 +72,11 @@ export default function Ziel() {
     return <LinearProgress />;
   }
 
+  const primaryType = destination.types[0];
+  const status = placeTypes[primaryType];
+  const DestinationIcon = status.Icon;
+  console.log('destination', destination, status); // eslint-disable-line
+
   return (
     <Container maxWidth="sm">
       <Box my={4}>
@@ -70,12 +85,12 @@ export default function Ziel() {
         </Button>
         <Grid container alignItems="center" className={classes.grid}>
           <Grid item>
-            <ShoppingCartIcon className={classes.icon} fontSize="large" />
+            <DestinationIcon className={classes.icon} fontSize="large" />
           </Grid>
           <Grid item xs>
             <Typography variant="h6">
               {destination.structured_formatting.main_text}
-              <CheckIcon className={classes.checkIcon} fontSize="inherit" />
+              {status.open && <CheckIcon className={classes.checkIcon} fontSize="inherit" />}
             </Typography>
             <Typography variant="body2">
               {destination.structured_formatting.secondary_text}
@@ -83,17 +98,55 @@ export default function Ziel() {
           </Grid>
         </Grid>
 
-        <Box className={classes.yay}>
-          Aktuell 16 Uhr: Weniger Besucher als gewöhnlich. Normalerweise bis zu 5 Min. Wartezeit.
-        </Box>
-
-        <Grid container justify="flex-end" className={classes.grid}>
-          <Grid item>
-            <Button variant="contained" color="primary" onClick={startNow}>
-              Jetzt losgehen
-            </Button>
-          </Grid>
-        </Grid>
+        {status.open && (
+          <>
+            <Box className={clsx(classes.status, classes.yay)}>
+              <Typography variant="body1">
+                <strong>Aktuell 16 Uhr:</strong>
+              </Typography>
+              <Typography variant="body1">
+                Weniger Besucher als gewöhnlich. Normalerweise bis zu 5 Min. Wartezeit.
+              </Typography>
+            </Box>
+            <Grid container justify="flex-end" className={classes.buttonContainer}>
+              <Grid item>
+                <Button variant="contained" color="primary" onClick={startNow}>
+                  Jetzt losgehen
+                </Button>
+              </Grid>
+            </Grid>
+          </>
+        )}
+        {!status.open && (
+          <>
+            <Box className={clsx(classes.status, classes.nay)}>
+              <Typography variant="body1">
+                <strong>Spar’ dir den Weg –</strong>
+              </Typography>
+              <Typography variant="body1">
+                leider geschlossen: Bis auf weiteres darf dieses Ziel keinen Publikumsverkehr
+                empfangen.
+              </Typography>
+            </Box>
+            <Typography variant="body1">
+              Die gute Nachricht? Es haben sich kürzlich tausende Menschen überlegt wie man die Zeit
+              zu Hause besser verbringen kann. Da ist bestimmt auch was für dich dabei!
+            </Typography>
+            <Grid container justify="flex-end" className={classes.buttonContainer}>
+              <Grid item>
+                <Button
+                  variant="outlined"
+                  color="primary"
+                  href="https://wirvsvirushackathon.org/"
+                  target="_blank"
+                  endIcon={<CallMadeIcon />}
+                >
+                  Mehr Infos
+                </Button>
+              </Grid>
+            </Grid>
+          </>
+        )}
       </Box>
       <Footer />
     </Container>
